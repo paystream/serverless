a3fc51dc2b40e17d06b97526a30930b4972a6284
lightshire <giansantillan18@gmail.com>
2016-02-17 01:56:19 +0000

    added helpers

---
+++ libraries/helpers/users/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    save: function (data) {
+
+    },
+
+    update: function (index, data) {
+
+    },
+
+    remove: function (index) {
+
+    },
+
+    get: function (index) {
+
+    }
+}
